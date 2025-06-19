ab32035aaadc46f229a211c9c00ed7d0adfa06ed
comtw8651 <comtw8651@gmail.com>
2025-06-19 14:09:03 +0800

    Delete login directory

--- login/spring1-1/src/main/java/com/example/demo/repository/VerificationTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.VerificationToken;
-
-public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
-    VerificationToken findByToken(String token);
-}
