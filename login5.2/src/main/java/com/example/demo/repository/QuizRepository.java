d8b2f5734990cacea713dd4b7d11d6bdf0f33b1a
comtw8651 <comtw8651@gmail.com>
2025-06-30 13:50:59 +0800

    Delete login5.2/src/main/java/com/example/demo/repository directory

--- login5.2/src/main/java/com/example/demo/repository/QuizRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Quiz;
-
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-} 
